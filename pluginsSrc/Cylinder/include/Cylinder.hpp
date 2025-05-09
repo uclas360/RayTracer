7dfcb0c1d348bc989dfa99a9cc26355e3341c5ca
sami <sami12022005@yahoo.fr>
2025-05-09 23:53:41 +0200

    feat: finite cylinder shape

---
+++ pluginsSrc/Cylinder/include/Cylinder.hpp
@@ -0,0 +1,42 @@
+/*
+** EPITECH PROJECT, 2025
+** B-OOP-400-PAR-4-1-raytracer-boris.cheng
+** File description:
+** Circle
+*/
+
+#ifndef CIRCLE_HPP
+#define CIRCLE_HPP
+
+#include <iostream>
+#include <libconfig.h++>
+
+#include "plugins/IShape.hpp"
+#include "Raytracer/Ray.hpp"
+#include "Raytracer/math/Vector.hpp"
+
+namespace RayTracer {
+
+class Cylinder : public IShape {
+    public:
+    Cylinder(const libconfig::Setting &);
+    HitRecord hits(const Ray &ray) const override;
+    void move(const Math::Vector3D &offset) override;
+    void rotate(const Math::Vector3D &angles) override;
+    void scale(size_t scale) override;
+    void setPosition(const Math::Vector3D &newPos) override;
+
+    private:
+    HitRecord hitsCapOnly(const Ray &ray) const;
+    Math::Vector3D pos_;
+    double radius_;
+    double height_;
+};
+
+std::ostream &operator<<(std::ostream &out, const Cylinder &ray);
+
+}  // namespace RayTracer
+
+
+
+#endif
---
+++ pluginsSrc/Cylinder/src/Cylinder.cpp
@@ -0,0 +1,117 @@
+/*
+** EPITECH PROJECT, 2025
+** B-OOP-400-PAR-4-1-raytracer-boris.cheng
+** File description:
+** Cylinder
+*/
+
+#include "../include/Cylinder.hpp"
+
+#include <cmath>
+#include <libconfig.h++>
+#include <ostream>
+
+#include "RaytracerCore.hpp"
+#include "Utils.hpp"
+#include "plugins/IShape.hpp"
+
+namespace RayTracer {
+
+Cylinder::Cylinder(const libconfig::Setting &settings) {
+  if (!settings.lookupValue("posX", this->pos_.x) ||
+      !settings.lookupValue("posY", this->pos_.y) ||
+      !settings.lookupValue("posZ", this->pos_.z) ||
+      !settings.lookupValue("radius", this->radius_) ||
+      !settings.lookupValue("height", this->height_)) {
+    throw std::runtime_error("Invalid configuration for Cylinder");
+  }
+}
+
+HitRecord Cylinder::hits(const Ray &ray) const {
+    Math::Vector3D oc = ray.pos - pos_;
+    double a = ray.dir.x * ray.dir.x + ray.dir.z * ray.dir.z;
+    double b = 2.0 * (oc.x * ray.dir.x + oc.z * ray.dir.z);
+    double c = oc.x * oc.x + oc.z * oc.z - radius_ * radius_;
+    
+    if (fabs(a) < EPSILON)
+        return hitsCapOnly(ray);
+    double discriminant = b * b - 4 * a * c;
+
+    if (discriminant < 0)
+        return hitsCapOnly(ray);
+    double sqrtd = sqrt(discriminant);
+    double t1 = (-b - sqrtd) / (2 * a);
+    double t2 = (-b + sqrtd) / (2 * a);
+    double t = t1;
+    double y1 = oc.y + t1 * ray.dir.y;
+
+    if (t1 < EPSILON || y1 < 0 || y1 > height_) {
+        t = t2;
+        double y2 = oc.y + t2 * ray.dir.y;
+        
+        if (t2 < EPSILON || y2 < 0 || y2 > height_)
+            return hitsCapOnly(ray);
+    }
+    Math::Vector3D p = ray.pos + ray.dir * t;
+    Math::Vector3D normal(p.x - pos_.x, 0, p.z - pos_.z);
+    normal = normal.normalized();
+    HitRecord capRecord = hitsCapOnly(ray);
+
+    if (capRecord.t > EPSILON && (capRecord.t < t || t < EPSILON))
+        return capRecord;
+    
+    return HitRecord(t, ray, *this, normal);
+}
+
+HitRecord Cylinder::hitsCapOnly(const Ray &ray) const {
+    double t_min = INFINITY;
+    Math::Vector3D normal;
+
+    if (fabs(ray.dir.y) > EPSILON) {
+        double t = (pos_.y - ray.pos.y) / ray.dir.y;
+        if (t > EPSILON) {
+            Math::Vector3D p = ray.pos + ray.dir * t;
+            double distance = (p - Math::Vector3D(pos_.x, pos_.y, pos_.z)).length();
+            if (distance <= radius_) {
+                t_min = t;
+                normal = Math::Vector3D(0, -1, 0);
+            }
+        }
+    }
+    if (fabs(ray.dir.y) > EPSILON) {
+        double t = (pos_.y + height_ - ray.pos.y) / ray.dir.y;
+        
+        if (t > EPSILON && t < t_min) {
+            Math::Vector3D p = ray.pos + ray.dir * t;
+            double distance = (p - Math::Vector3D(pos_.x, pos_.y + height_, pos_.z)).length();
+            
+            if (distance <= radius_) {
+                t_min = t;
+                normal = Math::Vector3D(0, 1, 0);
+            }
+        }
+    }
+    
+    if (t_min < INFINITY)
+        return HitRecord(t_min, ray, *this, normal);
+    
+    return HitRecord();
+}
+
+void Cylinder::move(const Math::Vector3D &offset) { this->pos_ += offset; }
+
+void Cylinder::rotate(const Math::Vector3D &angles) { (void)(angles); }
+
+void Cylinder::scale(size_t scale) { this->radius_ *= (double)scale; }
+
+void Cylinder::setPosition(const Math::Vector3D &newPos) {
+  this->pos_ = newPos;
+}
+
+}  // namespace RayTracer
+
+extern "C" {
+void *entry_point(const libconfig::Setting &config) {
+  return new RayTracer::Cylinder(config);
+}
+}
