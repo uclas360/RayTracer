5fa617eaca8b5bda93e27f1ad10be25a75e9498b
Laiheau <leo.barbier@epitech.eu>
2025-05-15 09:40:43 +0200

    feat: anti aliasing

--- include/Raytracer/Camera.hpp
+++ include/Raytracer/Camera.hpp
@@ -18,32 +18,32 @@
 namespace RayTracer {
 
 class Camera : public IObject {
- public:
-  Camera()
-      : screen_(Math::Vector3D(-0.5, -0.5, -1), Math::Vector3D(1, 0, 0),
-                Math::Vector3D(0, 1, 0)) {};
-  Camera(libconfig::Setting &);
+   public:
+    Camera()
+        : screen_(Math::Vector3D(-0.5, -0.5, -1), Math::Vector3D(1, 0, 0),
+                  Math::Vector3D(0, 1, 0)) {};
+    Camera(libconfig::Setting &);
 
-  void move(const Math::Vector3D &offset) override;
-  void rotate(const Math::Vector3D &angles) override;
-  void scale(size_t scale) override;
-  void setPosition(const Math::Vector3D &newPos) override;
-  void lookAt(const Math::Vector3D &target);
+    void move(const Math::Vector3D &offset) override;
+    void rotate(const Math::Vector3D &angles) override;
+    void scale(size_t scale) override;
+    void setPosition(const Math::Vector3D &newPos) override;
+    void lookAt(const Math::Vector3D &target);
 
-  Ray ray(double u, double v);
+    Ray ray(double u, double v, double xResolution, double yResolution);
 
- private:
-  void rotateX(double angle);
-  void rotateY(double angle);
-  void rotateZ(double angle);
+   private:
+    void rotateX(double angle);
+    void rotateY(double angle);
+    void rotateZ(double angle);
 
-  double rotationX = 0;
-  double rotationY = 0;
-  double rotationZ = 0;
+    double rotationX = 0;
+    double rotationY = 0;
+    double rotationZ = 0;
 
-  Rectangle screen_;
-  Math::Vector3D pos_;
-  Math::Vector3D rotation_;
+    Rectangle screen_;
+    Math::Vector3D pos_;
+    Math::Vector3D rotation_;
 };
 
 }  // namespace RayTracer
